3a64d0a1ba3721c0ea724e0de7861a9505a68fa5
Omar Delarosa <thedelarosa@gmail.com>
2019-02-07 08:09:03 -0500

    removing cloudflare ping from post-build

--- tasks/post-build.sh
+++ tasks/post-build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 # purge cloudflare cache
-echo "Purging CloudFlare cache..."
-curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
-     -H "X-Auth-Email: $CLOUDFLARE_AUTH_EMAIL" \
-     -H "X-Auth-Key: $CLOUDFLARE_AUTH_API_KEY" \
-     -H "Content-Type: application/json" \
-     --data '{"purge_everything":true}'
-echo "\n"
 
 echo "Uploading site maps to Google"
 # notify google of sitemap change
