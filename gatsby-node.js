5b0094b97267d59652af8535b371abf160536dbd
omar delarosa <thedelarosa@gmail.com>
2022-10-23 15:19:45 -0400

    adding pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,6 +10,9 @@ const MEDIA = 'MEDIA';
 const ASSET = 'ASSET';
 const NONE = 'NONE';
 
+const UPDATE_TAG = 'update';
+const PROCESS_TAG = 'process';
+
 const TYPES_MAPPING = {
     blog: POST,
     media: MEDIA,
@@ -28,17 +31,29 @@ function deriveTypeForNode(node, getNode) {
     }
 }
 
+function isNotBlogPage(p) {
+    {
+        // Exclude process posts
+        const tags = p.node.frontmatter.tags;
+        if (
+            _.includes(tags, PROCESS_TAG) &&
+            !_.includes(tags, UPDATE_TAG)
+        ) {
+            return true;
+        }
+        return false;
+    }
+}
+
 exports.createPages = ({ graphql, actions }) => {
     const { createPage, createRedirect } = actions;
 
     const blogPost = path.resolve('./src/templates/blog-post.js');
     const tagTemplate = path.resolve('./src/templates/tags.js');
-
-    console.log("REDIRECTS:", REDIRECTS);
+    const blogList = path.resolve("./src/templates/blog-list.js");
 
     // Create redirects
     REDIRECTS.forEach(redirect => {
-        console.log("REDIRECT", redirect);
         if (redirect.type === "post") {
             createRedirect({
                 fromPath: `/posts/${redirect.slug}`, 
@@ -93,6 +108,38 @@ exports.createPages = ({ graphql, actions }) => {
             });
         });
 
+        // Add pagination
+        const skippedSlugs = posts
+            .filter(isNotBlogPage)
+            .map(post => post?.node?.fields?.slug);
+        const postsPerPage = 5
+
+        console.log("skipped_slugs:", skippedSlugs);
+
+        // pages with removed ones
+        const numPages = Math.ceil((posts.length - skippedSlugs.length) / postsPerPage);
+
+        console.log('total_pagination_pages: ', numPages);
+
+        Array.from({ length: numPages }).forEach((_, i) => {
+            const url = `/page/${i + 1}`;
+            // Technically, page 0 doesn't need to be created.
+            // however, it's useful in case I ever change the index.js
+            // page to something else.
+            
+            createPage({
+                path: url,
+                component: blogList,
+                context: {
+                    limit: postsPerPage,
+                    skip: i * postsPerPage,
+                    numPages,
+                    currentPage: i + 1,
+                    skippedSlugs
+                },
+            })
+        })
+
         // Tag pages:
         let tags = [];
         // Iterate through each post, putting all found tags into `tags`
@@ -136,7 +183,6 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
         });
 
         const nodeType = deriveTypeForNode(node, getNode);
-        // console.log('NODE: ', node, nodeType);
         // Creates a type field
         createNodeField({
             name: 'type',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,83 +1,49 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Layout from '../components/Layout';
-import SEO from '../components/seo';
-import PostsList from '../components/PostsList';
-import Bio from '../components/Bio';
-import { PROCESS_TAG } from '../pages/process';
+
 import _ from 'lodash';
+import BlogList, { MAX_PER_PAGE } from '../templates/blog-list';
 
-const UPDATE_TAG = 'update';
-
-class Home extends React.Component {
+class Home extends React.Component  {
     render() {
         const { data } = this.props;
-        const { title: siteTitle } = data.site.siteMetadata;
-        const posts = data.allMarkdownRemark.edges.filter(p => {
-            // Exclude process posts
-            const tags = p.node.frontmatter.tags;
-            if (
-                _.includes(tags, PROCESS_TAG) &&
-                !_.includes(tags, UPDATE_TAG)
-            ) {
-                return false;
-            }
-            return true;
-        });
-        return (
-            <Layout location={this.props.location} title={siteTitle}>
-                <SEO
-                    title="Homepage | omardelarosa.com"
-                    keywords={[
-                        'blog',
-                        'gatsby',
-                        'javascript',
-                        'react',
-                        'software engineer',
-                        'developer',
-                        'musician',
-                        'technologist',
-                        'technology',
-                        'typescript',
-                    ]}
-                />
-                <PostsList posts={posts} />
-                <hr />
-                <Bio />
-            </Layout>
-        );
+        return <BlogList data={data} />
     }
 }
 
 export default Home;
 
 export const pageQuery = graphql`
-    query {
-        site {
-            siteMetadata {
-                title
-                author
-                sections {
-                    name
-                    path
-                }
-            }
-        }
-        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-            edges {
-                node {
-                    excerpt
-                    fields {
-                        slug
-                        url
-                    }
-                    frontmatter {
-                        date
-                        title
-                        tags
-                    }
-                }
+query blogListQuery {
+    site {
+        siteMetadata {
+            title
+            author
+            sections {
+                name
+                path
             }
         }
     }
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 5
+      skip: 0
+    ) {
+      edges {
+        node {
+            excerpt
+            fields {
+                slug
+                url
+            }
+            frontmatter {
+                date
+                title
+                tags
+            }
+        }
+      }
+    }
+  }
 `;
