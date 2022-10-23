fe61599ae666d9c4cdca0c1fc35eae4dbd1ab52a
omar delarosa <thedelarosa@gmail.com>
2022-10-23 15:22:13 -0400

    fixing import casing issue

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import PostsList from '../components/PostsList';
 import Bio from '../components/Bio';
 import SEO from '../components/seo';
