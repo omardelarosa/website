322dfb97c677d8e257154d783a79747d941815f6
omar <thedelarosa@gmail.com>
2025-10-14 22:22:14 -0400

    More TOML updates (#55)
    * specify base dir
    
    * adding cd to command
    
    * using npx for gatsby
    
    * restoring cd command
    
    * updating package-lock.json
    
    * Adding install command
    
    * updating gatsby version
    
    * removing npm install step from command
    
    * removing sitemap purge
    
    * adding npm install step
    
    * adding redirect for obsidian site
    
    * updating package name in obs-site

--- packages/gatsby-website/tasks/post-build.sh
+++ packages/gatsby-website/tasks/post-build.sh
@@ -2,6 +2,6 @@
 
 # purge cloudflare cache
 
-echo "Uploading site maps to Google"
+# echo "Uploading site maps to Google"
 # notify google of sitemap change
-curl 'http://www.google.com/webmasters/sitemaps/ping?sitemap=https://omardelarosa.com/sitemap.xml'
+# curl 'http://www.google.com/webmasters/sitemaps/ping?sitemap=https://omardelarosa.com/sitemap.xml'
